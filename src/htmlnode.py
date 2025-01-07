9435af3e9c46f0cf994b8a152ca566216a212a53
alex-infdev <alex.hojkolov@gmail.com>
2025-01-07 21:28:42 +0100

    added leafnode support

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,21 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("Invalid HTML: no value")
+        elif self.tag == None:
+            return(f"{self.value}")
+        if self.props != None:
+            print(f"{self.props}")
+            return(f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>")
+        else:
+            return(f"<{self.tag}>{self.value}</{self.tag}>")
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
