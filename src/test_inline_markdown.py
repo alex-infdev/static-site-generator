550bc01321161f71f793891d653a37f23d224f85
alex-infdev <alex.hojkolov@gmail.com>
2025-01-22 13:56:17 +0100

    added text to textnode function

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -145,6 +145,25 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/sMr36IX.jpeg) and a [link](https://www.google.com/)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/sMr36IX.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.google.com/"),
+            ],
+            nodes,
+        )
 
 
 if __name__ == "__main__":
