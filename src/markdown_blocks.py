c0de083b9a50127606a27b65c2f93475f644da9b
alex-infdev <alex.hojkolov@gmail.com>
2025-03-10 20:05:17 +0100

    add block to block types function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,4 +17,19 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
-            
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith("#"):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if all(line.startswith(">") for line in block.split("\n")):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in block.split("\n")):
+        return BlockType.UNORDERED_LIST
+
+    if all(line.split(".")[0].isdigit() and line.split(".")[1].startswith(" ") for line in lines):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
