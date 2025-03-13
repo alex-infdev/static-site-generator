8bb6236adc0d703df8e005af7cba45c17cc1858e
alex-infdev <alex.hojkolov@gmail.com>
2025-03-13 12:13:43 +0100

    add copy static dir functionality

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import os
+import shutil
 from textnode import *
+from copystatic import *
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-
-    test_node = TextNode("This is a text", TextType.BOLD, "https://www.google.com/")
-    print(test_node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    
+    print("Copying static files to public directory...")
+    copy_static(dir_path_static, dir_path_public)
 
 if __name__ == "__main__":
     main()
